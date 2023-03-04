13bd9c9ec90aa9ed93dc369cf1e6ee6d371e7400
ksy <tpdud3256@gmail.com>
2023-03-04 12:45:59 +0900

    drawer삭제, setting메뉴 tab메뉴로 이동

--- navigator/MainNavigator .js
+++ navigator/MainNavigator .js
@@ -1,19 +1,17 @@
 import { createStackNavigator } from "@react-navigation/stack";
 import React from "react";
 import Home from "../screen/main/home/Home";
-import DrawerNavigator from "./DrawerNavigator";
 import TabsNavigator from "./TabsNavigator";
 // import TabsNavigator from "./TabsNavigator";
 
 const Stack = createStackNavigator();
 
 export default function MainNavigator() {
-  return (
-    <Stack.Navigator
-      screenOptions={{ presentation: "card", headerShown: false }}
-    >
-      <Stack.Screen name="TabsNavigator" component={TabsNavigator} />
-      <Stack.Screen name="DrawerNavigator" component={DrawerNavigator} />
-    </Stack.Navigator>
-  );
+    return (
+        <Stack.Navigator
+            screenOptions={{ presentation: "card", headerShown: false }}
+        >
+            <Stack.Screen name="TabsNavigator" component={TabsNavigator} />
+        </Stack.Navigator>
+    );
 }
