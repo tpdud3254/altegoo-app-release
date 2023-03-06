01282885c8b7914b886ab56acfa9fed0c71a77aa
tpdud3254 <tpdud3254@naver.com>
2023-03-06 10:30:09 +0900

    regist post

--- screen/main/home/Home.js
+++ screen/main/home/Home.js
@@ -6,11 +6,11 @@ import SearchWork from "../search/SearchWork";
 import { ORDINARY } from "../../../constant";
 
 function Home() {
-    const { setIsLoggedIn } = useContext(LoginContext);
-    const { info, setInfo } = useContext(UserContext);
-    const navigation = useNavigation();
+  const { setIsLoggedIn } = useContext(LoginContext);
+  const { info, setInfo } = useContext(UserContext);
+  const navigation = useNavigation();
 
-    return <>{info.userType === ORDINARY ? null : <SearchWork />}</>;
+  return <>{info.userType === ORDINARY ? null : null}</>;
 }
 
 Home.propTypes = {};
