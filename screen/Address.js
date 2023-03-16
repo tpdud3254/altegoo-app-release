c8b713b90ca7f22668510e5f8628c4174cef051c
tpdud3254 <tpdud3254@burninggalaxy.com>
2023-03-16 17:52:40 +0900

    location문제 해결, 작업 등록 주소 로직 변경

--- screen/Address.js
+++ screen/Address.js
@@ -1,36 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { View } from "react-native";
 import Postcode from "@actbase/react-daum-postcode";
 import { REGIST_NAV } from "../constant";
 
 function Address({ navigation, route }) {
-    const onSelected = (data) => {
-        let addressArr = [...route?.params?.addressArr] || ["", ""];
+  console.log("Address Start: ", route.params.addressIndex);
 
-        if (route?.params?.data === 0) {
-            addressArr[0] = data.address;
-        } else {
-            addressArr[1] = data.address;
-        }
-        navigation.navigate(REGIST_NAV[2], { addressArr: [...addressArr] }); //TODO:각 상세주소로 포커스 이동하게 하기
-        // Toast.show({
-        //   type: "errorToast", //TODO: normal toast로 변경
-        //   props: "상세 주소를 입력해주세요.",
-        // });
-    };
-    const YourView = () => (
-        <Postcode
-            style={{ width: "100%", height: "100%", paddingTop: 50 }}
-            jsOptions={{ animation: true }}
-            onSelected={(data) => onSelected(data)}
-        />
-    );
+  const onSelected = (data) => {
+    console.log("onSelected : ", data);
+    const address = data.address;
+    const sido = data.sido;
+    const sigungu = data.sigungu;
 
-    return (
-        <View>
-            <YourView />
-        </View>
-    );
+    if (route?.params?.addressIndex || route?.params?.addressIndex >= 0) {
+      console.log("a");
+      if (route?.params?.addressIndex === 0) {
+        console.log("b");
+        navigation.navigate(REGIST_NAV[2], {
+          ...route?.params,
+          selectAddress1: { address, sido, sigungu },
+        });
+      } else
+        navigation.navigate(REGIST_NAV[2], {
+          ...route?.params,
+          selectAddress2: { address, sido, sigungu },
+        });
+    } //TODO:각 상세주소로 포커스 이동하게 하기
+    // Toast.show({
+    //   type: "errorToast", //TODO: normal toast로 변경
+    //   props: "상세 주소를 입력해주세요.",
+    // });
+  };
+  const YourView = () => (
+    <Postcode
+      style={{ width: "100%", height: "100%", paddingTop: 50 }}
+      jsOptions={{ animation: true }}
+      onSelected={(data) => onSelected(data)}
+    />
+  );
+
+  return (
+    <View>
+      <YourView />
+    </View>
+  );
 }
 
 export default Address;
--- screen/main/regist/SearchAddress.js
+++ screen/main/regist/SearchAddress.js
@@ -1,8 +1,8 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import styled from "styled-components/native";
 import KakaoButton, {
-    ButtonContainer,
+  ButtonContainer,
 } from "../../../component/button/KakaoButton";
 import PlainButton from "../../../component/button/PlainButton";
 import MainLayout from "../../../component/layout/MainLayout";
@@ -14,207 +14,260 @@ import { Toast } from "react-native-toast-message/lib/src/Toast";
 import { REGIST_NAV } from "../../../constant";
 
 const Container = styled.KeyboardAvoidingView`
-    flex: 1;
+  flex: 1;
 `;
 
 const InputAddress = styled.View`
-    background-color: white;
-    border-radius: 10px;
-    padding: 10px;
+  background-color: white;
+  border-radius: 10px;
+  padding: 10px;
 `;
 
 const InputOtherAddress = styled(InputAddress)`
-    margin-top: 15px;
+  margin-top: 15px;
 `;
 const HelpWrapper = styled.View`
-    flex-direction: row;
+  flex-direction: row;
 `;
 
 const InputAddWrapper = styled.View`
-    margin-top: 10px;
+  margin-top: 10px;
 `;
 const Search = styled.TouchableOpacity`
-    border: 1px solid ${theme.boxColor};
-    padding: 8px 5px;
+  border: 1px solid ${theme.boxColor};
+  padding: 8px 5px;
 `;
 const DetailAddress = styled.TextInput`
-    border: 1px solid ${theme.boxColor};
-    padding: 8px 5px;
-    font-size: 20px;
-    font-weight: 300;
-    margin-top: 8px;
+  border: 1px solid ${theme.boxColor};
+  padding: 8px 5px;
+  font-size: 20px;
+  font-weight: 300;
+  margin-top: 8px;
 `;
 function SearchAddress({ route, navigation }) {
-    const { registInfo, setRegistInfo } = useContext(RegistContext);
-    const { setValue, register, handleSubmit } = useForm();
+  const { registInfo, setRegistInfo } = useContext(RegistContext);
+  const { setValue, register, handleSubmit } = useForm();
+  console.log("registInfo : ", registInfo);
 
-    console.log("registInfo : ", registInfo);
+  useEffect(() => {
+    register("detailAddress1");
+    register("detailAddress2");
+  }, []);
 
-    useEffect(() => {
-        register("detailAddress1");
-        register("detailAddress2");
-    }, []);
+  console.log("SearchAddress Start: ", route?.params);
 
-    const getHelpText = (upDown) => {
-        if (upDown === "양사") {
-            if (registInfo.bothType === 1) return "내림";
-            else return "올림";
-        } else {
-            return upDown;
-        }
-    };
+  const getHelpText = (upDown) => {
+    if (upDown === "양사") {
+      if (registInfo.bothType === 1) return "내림";
+      else return "올림";
+    } else {
+      return upDown;
+    }
+  };
 
-    const searchAddress = (index) => {
-        navigation.navigate("Address", {
-            data: index,
-            addressArr: route?.params?.addressArr || ["", ""],
+  const searchAddress = (index) => {
+    navigation.navigate("Address", {
+      ...route?.params,
+      addressIndex: index,
+    });
+  };
+
+  const getRegion = (sido, sigungu) => {
+    if (sido.includes("서울")) return 1;
+    else if (sido.includes("인천")) return 2;
+    else if (sido.includes("경기")) {
+      if (
+        sigungu.includes("김포") ||
+        sigungu.includes("부천") ||
+        sigungu.includes("파주") ||
+        sigungu.includes("고양") ||
+        sigungu.includes("동두천") ||
+        sigungu.includes("연천")
+      )
+        return 3;
+      else if (
+        sigungu.includes("의정부") ||
+        sigungu.includes("양주") ||
+        sigungu.includes("구리") ||
+        sigungu.includes("남양주") ||
+        sigungu.includes("포천") ||
+        sigungu.includes("가평")
+      )
+        return 4;
+      else if (
+        sigungu.includes("광명") ||
+        sigungu.includes("시흥") ||
+        sigungu.includes("안산") ||
+        sigungu.includes("안양") ||
+        sigungu.includes("과천") ||
+        sigungu.includes("의왕") ||
+        sigungu.includes("군포") ||
+        sigungu.includes("수원") ||
+        sigungu.includes("오산") ||
+        sigungu.includes("화성") ||
+        sigungu.includes("평택")
+      )
+        return 5;
+      else if (
+        sigungu.includes("성남") ||
+        sigungu.includes("하남") ||
+        sigungu.includes("광주") ||
+        sigungu.includes("용인") ||
+        sigungu.includes("안성") ||
+        sigungu.includes("이천") ||
+        sigungu.includes("여주") ||
+        sigungu.includes("양평")
+      )
+        return 6;
+    }
+  };
+  const onNextStep = ({ detailAddress1, detailAddress2 }) => {
+    if (registInfo.upDown === "양사") {
+      if (
+        !route?.params?.selectAddress1 ||
+        route?.params?.selectAddress1 === "" ||
+        !detailAddress1 ||
+        detailAddress1 === "" ||
+        !route?.params?.selectAddress2 ||
+        route?.params?.selectAddress2 === "" ||
+        !detailAddress2 ||
+        detailAddress2 === ""
+      ) {
+        //TODO: 상세주소 없음 추가
+        Toast.show({
+          type: "errorToast",
+          props: "주소를 입력해주세요.",
         });
-    };
+        return;
+      }
 
-    const onNextStep = ({ detailAddress1, detailAddress2 }) => {
-        if (registInfo.upDown === "양사") {
-            if (
-                route?.params?.addressArr[0] === "" ||
-                !detailAddress1 ||
-                detailAddress1 === "" ||
-                route?.params?.addressArr[1] === "" ||
-                !detailAddress2 ||
-                detailAddress2 === ""
-            ) {
-                //TODO: 상세주소 없음 추가
-                Toast.show({
-                    type: "errorToast",
-                    props: "주소를 입력해주세요.",
-                });
-                return;
-            }
+      const address1 =
+        route?.params?.selectAddress1?.address + " " + detailAddress1;
 
-            const Address1 =
-                route?.params?.addressArr[0] + " " + detailAddress1;
+      const address2 =
+        route?.params?.selectAddress2?.address + " " + detailAddress2;
 
-            const Address2 =
-                route?.params?.addressArr[1] + " " + detailAddress2;
+      setRegistInfo({
+        address1,
+        simpleAddress1: `${route?.params?.selectAddress1?.sido} ${route?.params?.selectAddress1?.sigungu}`,
+        address2,
+        simpleAddress2: `${route?.params?.selectAddress2?.sido} ${route?.params?.selectAddress2?.sigungu}`,
+        region: getRegion(
+          route?.params?.selectAddress1?.sido,
+          route?.params?.selectAddress1?.sigungu
+        ),
+        ...registInfo,
+      });
+    } else {
+      if (
+        !route?.params?.selectAddress1 ||
+        route?.params?.selectAddress1 === "" ||
+        !detailAddress1 ||
+        detailAddress1 === ""
+      ) {
+        //TODO: 상세주소 없음 추가
+        Toast.show({
+          type: "errorToast",
+          props: "주소를 입력해주세요.",
+        });
+        return;
+      }
 
-            setRegistInfo({
-                address: Address1,
-                otherAddress: Address2,
-                ...registInfo,
-            });
-        } else {
-            if (
-                route?.params?.addressArr[0] === "" ||
-                !detailAddress1 ||
-                detailAddress1 === ""
-            ) {
-                //TODO: 상세주소 없음 추가
-                Toast.show({
-                    type: "errorToast",
-                    props: "주소를 입력해주세요.",
-                });
-                return;
-            }
+      const address1 =
+        route?.params?.selectAddress1?.address + " " + detailAddress1;
 
-            const Address1 =
-                route?.params?.addressArr[0] + " " + detailAddress1;
+      setRegistInfo({
+        address1,
+        simpleAddress1: `${route?.params?.selectAddress1?.sido} ${route?.params?.selectAddress1?.sigungu}`,
+        address2: null,
+        simpleAddress2: null,
+        region: getRegion(
+          route?.params?.selectAddress1?.sido,
+          route?.params?.selectAddress1?.sigungu
+        ),
+        ...registInfo,
+      });
+    }
 
-            setRegistInfo({
-                address: Address1,
-                otherAddress: null,
-                ...registInfo,
-            });
-        }
+    navigation.navigate(REGIST_NAV[3]);
+  };
 
-        navigation.navigate(REGIST_NAV[3]);
-    };
-
-    const Help = ({ number, text }) => (
-        <HelpWrapper>
-            <MaterialCommunityIcons
-                name={`numeric-${number}-circle-outline`}
-                size={30}
-                color="#777777"
+  const Help = ({ number, text }) => (
+    <HelpWrapper>
+      <MaterialCommunityIcons
+        name={`numeric-${number}-circle-outline`}
+        size={30}
+        color="#777777"
+      />
+      <PlainText style={{ marginLeft: 5, color: "#777777" }}>{text}</PlainText>
+    </HelpWrapper>
+  );
+  // TODO: 경기도, 인천, 서울 이외에는 막기
+  return (
+    <MainLayout>
+      <Container behavior="position" keyboardVerticalOffset={0}>
+        <InputAddress>
+          <Help
+            number="1"
+            text={`'${getHelpText(registInfo.upDown)}' 주소 검색하기`}
+          />
+          <InputAddWrapper>
+            <Search onPress={() => searchAddress(0)}>
+              {route?.params?.selectAddress1?.address &&
+              route?.params?.selectAddress1?.address !== "" ? (
+                <PlainText>{route?.params?.selectAddress1?.address}</PlainText>
+              ) : (
+                <PlainText style={{ color: "#777777" }}>
+                  주소 입력하기
+                </PlainText>
+              )}
+            </Search>
+            <DetailAddress
+              placeholder="상세주소 입력하기"
+              onChangeText={(text) => setValue("detailAddress1", text)}
             />
-            <PlainText style={{ marginLeft: 5, color: "#777777" }}>
-                {text}
-            </PlainText>
-        </HelpWrapper>
-    );
-    // TODO: 경기도, 인천, 서울 이외에는 막기
-    return (
-        <MainLayout>
-            <Container behavior="position" keyboardVerticalOffset={0}>
-                <InputAddress>
-                    <Help
-                        number="1"
-                        text={`'${getHelpText(
-                            registInfo.upDown
-                        )}' 주소 검색하기`}
-                    />
-                    <InputAddWrapper>
-                        <Search onPress={() => searchAddress(0)}>
-                            {route?.params?.addressArr &&
-                            route?.params?.addressArr[0] !== "" ? (
-                                <PlainText>
-                                    {route?.params?.addressArr[0]}
-                                </PlainText>
-                            ) : (
-                                <PlainText style={{ color: "#777777" }}>
-                                    주소 입력하기
-                                </PlainText>
-                            )}
-                        </Search>
-                        <DetailAddress
-                            placeholder="상세주소 입력하기"
-                            onChangeText={(text) =>
-                                setValue("detailAddress1", text)
-                            }
-                        />
-                    </InputAddWrapper>
-                </InputAddress>
+          </InputAddWrapper>
+        </InputAddress>
 
-                {registInfo.upDown === "양사" ? (
-                    <InputOtherAddress>
-                        <Help
-                            number="2"
-                            text={`'${
-                                getHelpText(registInfo.upDown) === "내림"
-                                    ? "올림"
-                                    : "내림"
-                            }' 주소 검색하기`}
-                        />
-                        <InputAddWrapper>
-                            <Search onPress={() => searchAddress(1)}>
-                                {route?.params?.addressArr &&
-                                route?.params?.addressArr[1] !== "" ? (
-                                    <PlainText>
-                                        {route?.params?.addressArr[1]}
-                                    </PlainText>
-                                ) : (
-                                    <PlainText style={{ color: "#777777" }}>
-                                        주소 입력하기
-                                    </PlainText>
-                                )}
-                            </Search>
-                            <DetailAddress
-                                placeholder="상세주소 입력하기"
-                                onChangeText={(text) =>
-                                    setValue("detailAddress2", text)
-                                }
-                            />
-                        </InputAddWrapper>
-                    </InputOtherAddress>
-                ) : null}
-            </Container>
-            <ButtonContainer>
-                <PlainButton
-                    text="다음단계"
-                    style={{ width: "80%" }}
-                    onPress={handleSubmit(onNextStep)}
-                />
-                <KakaoButton />
-            </ButtonContainer>
-        </MainLayout>
-    );
+        {registInfo.upDown === "양사" ? (
+          <InputOtherAddress>
+            <Help
+              number="2"
+              text={`'${
+                getHelpText(registInfo.upDown) === "내림" ? "올림" : "내림"
+              }' 주소 검색하기`}
+            />
+            <InputAddWrapper>
+              <Search onPress={() => searchAddress(1)}>
+                {route?.params?.selectAddress2?.address &&
+                route?.params?.selectAddress2?.address !== "" ? (
+                  <PlainText>
+                    {route?.params?.selectAddress2?.address}
+                  </PlainText>
+                ) : (
+                  <PlainText style={{ color: "#777777" }}>
+                    주소 입력하기
+                  </PlainText>
+                )}
+              </Search>
+              <DetailAddress
+                placeholder="상세주소 입력하기"
+                onChangeText={(text) => setValue("detailAddress2", text)}
+              />
+            </InputAddWrapper>
+          </InputOtherAddress>
+        ) : null}
+      </Container>
+      <ButtonContainer>
+        <PlainButton
+          text="다음단계"
+          style={{ width: "80%" }}
+          onPress={handleSubmit(onNextStep)}
+        />
+        <KakaoButton />
+      </ButtonContainer>
+    </MainLayout>
+  );
 }
 
 export default SearchAddress;
