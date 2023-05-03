79c27b6ce265b59f3071448862df3409f6476e5e
tpdud3254 <tpdud3254@burninggalaxy.com>
2023-05-03 09:12:49 +0900

    location granted 코드  App.js로 이동

--- App.js
+++ App.js
@@ -2,19 +2,19 @@ import React, { useCallback, useEffect, useState } from "react";
 import { Image, View } from "react-native";
 import * as SplashScreen from "expo-splash-screen";
 import {
-    useFonts,
-    IBMPlexSansKR_100Thin,
-    IBMPlexSansKR_200ExtraLight,
-    IBMPlexSansKR_300Light,
-    IBMPlexSansKR_400Regular,
-    IBMPlexSansKR_500Medium,
-    IBMPlexSansKR_600SemiBold,
-    IBMPlexSansKR_700Bold,
+  useFonts,
+  IBMPlexSansKR_100Thin,
+  IBMPlexSansKR_200ExtraLight,
+  IBMPlexSansKR_300Light,
+  IBMPlexSansKR_400Regular,
+  IBMPlexSansKR_500Medium,
+  IBMPlexSansKR_600SemiBold,
+  IBMPlexSansKR_700Bold,
 } from "@expo-google-fonts/ibm-plex-sans-kr";
 import {
-    NanumGothic_400Regular,
-    NanumGothic_700Bold,
-    NanumGothic_800ExtraBold,
+  NanumGothic_400Regular,
+  NanumGothic_700Bold,
+  NanumGothic_800ExtraBold,
 } from "@expo-google-fonts/nanum-gothic";
 import Toast from "react-native-toast-message";
 import { LoginProvider } from "./context/LoginContext";
@@ -25,113 +25,130 @@ import { StatusBar } from "expo-status-bar";
 import { RegistProvider } from "./context/RegistContext";
 import { speech } from "./utils";
 import { Asset } from "expo-asset";
+import * as Location from "expo-location";
 
 SplashScreen.preventAutoHideAsync();
 
 export default function App() {
-    const [appIsReady, setAppIsReady] = useState(false);
-    const [fontsLoaded] = useFonts({
-        IBMPlexSansKR_100Thin,
-        IBMPlexSansKR_200ExtraLight,
-        IBMPlexSansKR_300Light,
-        IBMPlexSansKR_400Regular,
-        IBMPlexSansKR_500Medium,
-        IBMPlexSansKR_600SemiBold,
-        IBMPlexSansKR_700Bold,
-        NanumGothic_400Regular,
-        NanumGothic_700Bold,
-        NanumGothic_800ExtraBold,
+  const [appIsReady, setAppIsReady] = useState(false);
+  const [locationGranted, setLocationGranted] = useState(true);
+  const [pushGranted, setPushGranted] = useState(true);
+  const [fontsLoaded] = useFonts({
+    IBMPlexSansKR_100Thin,
+    IBMPlexSansKR_200ExtraLight,
+    IBMPlexSansKR_300Light,
+    IBMPlexSansKR_400Regular,
+    IBMPlexSansKR_500Medium,
+    IBMPlexSansKR_600SemiBold,
+    IBMPlexSansKR_700Bold,
+    NanumGothic_400Regular,
+    NanumGothic_700Bold,
+    NanumGothic_800ExtraBold,
+  });
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        const imageAssets = cacheImages([
+          require(`./assets/images/intro/img_01.png`),
+          require(`./assets/images/intro/img_02.png`),
+          require(`./assets/images/intro/img_03.png`),
+        ]);
+
+        await Promise.all([...imageAssets]);
+
+        askLocationPermission();
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    }
+
+    const askLocationPermission = async () => {
+      const { granted } = await Location.requestForegroundPermissionsAsync();
+      if (!granted) {
+        setLocationGranted(false);
+      }
+    };
+
+    function createSocket() {
+      const ws = new WebSocket(`wss://altegoo.shop`);
+      // const ws = new WebSocket(`wss://cab2-211-59-182-118.jp.ngrok.io`);
+
+      ws.onopen = (e) => {
+        // connection opened
+        console.log("connected");
+
+        // send a message
+        // ws.send("hello");
+      };
+
+      ws.onmessage = (e) => {
+        // a message was received
+        console.log("message : ", e.data);
+        const parsed = JSON.parse(e.data);
+
+        if (parsed.type === "REGIST") {
+          speech(parsed.tts_msg, parsed.exceptionUserId);
+        }
+      };
+
+      ws.onerror = (e) => {
+        // an error occurred
+        console.log("ws.onerror:", e.message);
+      };
+
+      ws.onclose = (e) => {
+        // connection closed
+        console.log("ws.onclose:", e);
+        // setTimeout(() => createSocket(), 1000);
+      };
+
+      // ws.close();
+    }
+
+    // export const ws = new WebSocket(`wss://altegoo.shop`);
+
+    createSocket();
+    prepare();
+  }, []);
+
+  function cacheImages(images) {
+    return images.map((image) => {
+      if (typeof image === "string") {
+        return Image.prefetch(image);
+      } else {
+        return Asset.fromModule(image).downloadAsync();
+      }
     });
+  }
 
-    useEffect(() => {
-        async function prepare() {
-            try {
-                const imageAssets = cacheImages([
-                    require(`./assets/images/intro/img_01.png`),
-                    require(`./assets/images/intro/img_02.png`),
-                    require(`./assets/images/intro/img_03.png`),
-                ]);
-
-                await Promise.all([...imageAssets]);
-            } catch (e) {
-                console.warn(e);
-            } finally {
-                setAppIsReady(true);
-            }
-        }
-        function createSocket() {
-            const ws = new WebSocket(`wss://altegoo.shop`);
-            // const ws = new WebSocket(`wss://cab2-211-59-182-118.jp.ngrok.io`);
-
-            ws.onopen = (e) => {
-                // connection opened
-                console.log("connected");
-
-                // send a message
-                // ws.send("hello");
-            };
-
-            ws.onmessage = (e) => {
-                // a message was received
-                console.log("message : ", e.data);
-                const parsed = JSON.parse(e.data);
-
-                if (parsed.type === "REGIST") {
-                    speech(parsed.tts_msg, parsed.exceptionUserId);
-                }
-            };
-
-            ws.onerror = (e) => {
-                // an error occurred
-                console.log("ws.onerror:", e.message);
-            };
-
-            ws.onclose = (e) => {
-                // connection closed
-                console.log("ws.onclose:", e);
-                // setTimeout(() => createSocket(), 1000);
-            };
-
-            // ws.close();
-        }
-
-        // export const ws = new WebSocket(`wss://altegoo.shop`);
-
-        createSocket();
-        prepare();
-    }, []);
-
-    function cacheImages(images) {
-        return images.map((image) => {
-            if (typeof image === "string") {
-                return Image.prefetch(image);
-            } else {
-                return Asset.fromModule(image).downloadAsync();
-            }
-        });
+  const onLayoutRootView = useCallback(async () => {
+    if (!locationGranted || !pushGranted) {
+      //TODO:앱종료 추가
+      return null;
     }
-
-    const onLayoutRootView = useCallback(async () => {
-        if (appIsReady && fontsLoaded) {
-            await SplashScreen.hideAsync();
-        }
-    }, [appIsReady, fontsLoaded]);
-
-    if (!appIsReady || !fontsLoaded) {
-        return null;
+    if (appIsReady && fontsLoaded) {
+      await SplashScreen.hideAsync();
     }
+  }, [appIsReady, fontsLoaded]);
 
-    return (
-        <View style={{ flex: 1 }} onLayout={onLayoutRootView}>
-            <LoginProvider>
-                <UserProvider>
-                    <RegistProvider>
-                        <RootNavigator />
-                    </RegistProvider>
-                </UserProvider>
-            </LoginProvider>
-            <StatusBar style="dark" />
-            <Toast position="bottom" bottomOffset="100" config={toastConfig} />
-        </View>
-    );
+  if (!appIsReady || !fontsLoaded) {
+    return null;
+  }
+
+  return (
+    <View style={{ flex: 1 }} onLayout={onLayoutRootView}>
+      <LoginProvider>
+        <UserProvider>
+          <RegistProvider>
+            <RootNavigator />
+          </RegistProvider>
+        </UserProvider>
+      </LoginProvider>
+      <StatusBar style="dark" />
+      <Toast position="bottom" bottomOffset="100" config={toastConfig} />
+    </View>
+  );
 }
--- screen/Intro.js
+++ screen/Intro.js
@@ -1,157 +1,123 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useRef, useState } from "react";
 import styled from "styled-components/native";
 import { FlatList, useWindowDimensions } from "react-native";
-import * as Location from "expo-location";
 import { useNavigation } from "@react-navigation/native";
 import { color } from "../styles";
 import Button from "../component/button/Button";
 
 const imagePath = [
-    require(`../assets/images/intro/img_01.png`),
-    require(`../assets/images/intro/img_02.png`),
-    require(`../assets/images/intro/img_03.png`),
+  require(`../assets/images/intro/img_01.png`),
+  require(`../assets/images/intro/img_02.png`),
+  require(`../assets/images/intro/img_03.png`),
 ];
 
 const Container = styled.View`
-    flex: 1;
-    width: 100%;
-    background-color: white;
+  flex: 1;
+  width: 100%;
+  background-color: white;
 `;
 
 const Wrapper = styled.View`
-    flex: 10;
-    align-items: center;
-    margin-top: 120px;
+  flex: 10;
+  align-items: center;
+  margin-top: 120px;
 `;
 
 const Bottom = styled.View`
-    flex: 1.5;
+  flex: 1.5;
 `;
 
 const IntroImage = styled.Image`
-    width: ${(props) => props.size + "px"};
-    height: ${(props) => props.size + "px"};
+  width: ${(props) => props.size + "px"};
+  height: ${(props) => props.size + "px"};
 `;
 
 const Indicators = styled.View`
-    flex-direction: row;
-    justify-content: center;
-    margin-bottom: 35px;
+  flex-direction: row;
+  justify-content: center;
+  margin-bottom: 35px;
 `;
 
 const Indicator = styled.TouchableOpacity`
-    width: ${(props) => (props.cur ? "35px" : "13px")};
-    height: 13px;
-    background-color: ${(props) => props.color || color.main};
-    justify-content: center;
-    align-items: center;
-    border-radius: 100px;
-    margin: 0px 5px 0px 5px;
+  width: ${(props) => (props.cur ? "35px" : "13px")};
+  height: 13px;
+  background-color: ${(props) => props.color || color.main};
+  justify-content: center;
+  align-items: center;
+  border-radius: 100px;
+  margin: 0px 5px 0px 5px;
 `;
 
 const Buttons = styled.View`
-    flex-direction: row;
-    align-items: center;
-    justify-content: space-evenly;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-evenly;
 `;
 
 function Intro() {
-    const { width: imageSize } = useWindowDimensions();
-    const navigation = useNavigation();
-    const flatListRef = useRef();
-    const [imageIndex, setImageIndex] = useState(0);
-    const [ok, setOk] = useState(true);
+  const { width: imageSize } = useWindowDimensions();
+  const navigation = useNavigation();
+  const flatListRef = useRef();
+  const [imageIndex, setImageIndex] = useState(0);
 
-    const askLocationPermission = async () => {
-        const { granted } = await Location.requestForegroundPermissionsAsync();
-        if (!granted) {
-            setOk(false);
-        }
-    };
+  const renderIntro = ({ item: path }) => (
+    <IntroImage size={imageSize} source={path} />
+  );
 
-    useEffect(() => {
-        askLocationPermission();
-    }, []);
+  const scrollToIntroImage = (index) => {
+    flatListRef.current.scrollToIndex({ index });
+    setImageIndex(index);
+  };
 
-    const renderIntro = ({ item: path }) => (
-        <IntroImage size={imageSize} source={path} />
-    );
+  const goToSignIn = () => {
+    navigation.navigate("SignInNavigator");
+  };
 
-    const scrollToIntroImage = (index) => {
-        flatListRef.current.scrollToIndex({ index });
-        setImageIndex(index);
-    };
-
-    const goToSignIn = () => {
-        navigation.navigate("SignInNavigator");
-    };
-
-    const goToSignUp = () => {
-        navigation.navigate("SignUpNavigator");
-    };
-    return (
-        <>
-            {
-                ok ? (
-                    <Container>
-                        <Wrapper>
-                            <FlatList
-                                horizontal
-                                pagingEnabled
-                                showsHorizontalScrollIndicator={false}
-                                data={imagePath}
-                                renderItem={renderIntro}
-                                ref={flatListRef}
-                                onMomentumScrollEnd={(event) => {
-                                    const index = Math.floor(
-                                        Math.floor(
-                                            event.nativeEvent.contentOffset.x
-                                        ) /
-                                            Math.floor(
-                                                event.nativeEvent
-                                                    .layoutMeasurement.width
-                                            )
-                                    );
-                                    setImageIndex(index);
-                                }}
-                            />
-                            <Indicators>
-                                {imagePath.map((__, index) => (
-                                    <Indicator
-                                        key={index}
-                                        onPress={() =>
-                                            scrollToIntroImage(index)
-                                        }
-                                        color={
-                                            imageIndex === index
-                                                ? color.btnAccent
-                                                : color.btnDisable
-                                        }
-                                        cur={imageIndex === index}
-                                    />
-                                ))}
-                            </Indicators>
-                        </Wrapper>
-                        <Bottom>
-                            <Buttons>
-                                <Button
-                                    onPress={goToSignIn}
-                                    type="accent"
-                                    style={{ width: 185 }}
-                                    text="로그인"
-                                />
-                                <Button
-                                    onPress={goToSignUp}
-                                    style={{ width: 185 }}
-                                    text="회원가입"
-                                />
-                            </Buttons>
-                        </Bottom>
-                    </Container>
-                ) : null //TODO: 어플종료 추가
-            }
-        </>
-    );
+  const goToSignUp = () => {
+    navigation.navigate("SignUpNavigator");
+  };
+  return (
+    <Container>
+      <Wrapper>
+        <FlatList
+          horizontal
+          pagingEnabled
+          showsHorizontalScrollIndicator={false}
+          data={imagePath}
+          renderItem={renderIntro}
+          ref={flatListRef}
+          onMomentumScrollEnd={(event) => {
+            const index = Math.floor(
+              Math.floor(event.nativeEvent.contentOffset.x) /
+                Math.floor(event.nativeEvent.layoutMeasurement.width)
+            );
+            setImageIndex(index);
+          }}
+        />
+        <Indicators>
+          {imagePath.map((__, index) => (
+            <Indicator
+              key={index}
+              onPress={() => scrollToIntroImage(index)}
+              color={imageIndex === index ? color.btnAccent : color.btnDisable}
+              cur={imageIndex === index}
+            />
+          ))}
+        </Indicators>
+      </Wrapper>
+      <Bottom>
+        <Buttons>
+          <Button
+            onPress={goToSignIn}
+            type="accent"
+            style={{ width: 185 }}
+            text="로그인"
+          />
+          <Button onPress={goToSignUp} style={{ width: 185 }} text="회원가입" />
+        </Buttons>
+      </Bottom>
+    </Container>
+  );
 }
 
 export default Intro;
