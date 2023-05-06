d22d6455e68a9f0e4f5a5ce2852404fc454eae06
ksy <tpdud3256@gmail.com>
2023-05-06 16:03:16 +0900

    push 알림  추가

--- navigator/Main/MainNavigator.js
+++ navigator/Main/MainNavigator.js
@@ -16,307 +16,334 @@ import { getAsyncStorageToken } from "../../utils";
 import TabsNavigator from "./TabsNavigator";
 import * as Device from "expo-device";
 import * as Notifications from "expo-notifications";
+import * as TaskManager from "expo-task-manager";
 import { Platform } from "react-native";
+import {
+    AndroidNotificationPriority,
+    AndroidNotificationVisibility,
+} from "expo-notifications";
+import { color } from "../../styles";
 
 const Stack = createStackNavigator();
 
 Notifications.setNotificationHandler({
-  handleNotification: async () => ({
-    shouldShowAlert: true,
-    shouldPlaySound: true,
-    shouldSetBadge: true,
-  }),
+    handleNotification: async () => ({
+        shouldShowAlert: true,
+        shouldPlaySound: true,
+        shouldSetBadge: true,
+        priority: AndroidNotificationPriority.MAX,
+    }),
 });
 
+const BACKGROUND_NOTIFICATION_TASK = "BACKGROUND-NOTIFICATION-TASK";
+
+TaskManager.defineTask(
+    BACKGROUND_NOTIFICATION_TASK,
+    ({ data, error, executionInfo }) => {
+        console.log("Received a notification in the background!");
+        // Do something with the notification data
+    }
+);
+
+Notifications.registerTaskAsync(BACKGROUND_NOTIFICATION_TASK);
+
 export default function MainNavigator() {
-  const [loading, setLoading] = useState(true);
-  const { firstLogin } = useContext(LoginContext);
-  const [acceptOrderList, setAcceptOrderList] = useState([]);
-  const [registOrderList, setRegistOrderList] = useState([]);
-  const [notification, setNotification] = useState(false);
-  const notificationListener = useRef();
-  const responseListener = useRef();
+    const [loading, setLoading] = useState(true);
+    const { firstLogin } = useContext(LoginContext);
+    const [acceptOrderList, setAcceptOrderList] = useState([]);
+    const [registOrderList, setRegistOrderList] = useState([]);
+    const [notification, setNotification] = useState(false);
+    const notificationListener = useRef();
+    const responseListener = useRef();
 
-  useEffect(() => {
-    registerForPushNotificationsAsync().then(async (token) => {
-      try {
-        const response = await axios.post(
-          SERVER + "/push/token",
-          {
-            token,
-          },
-          {
-            headers: {
-              auth: await getAsyncStorageToken(),
-            },
-          }
-        );
+    useEffect(() => {
+        registerForPushNotificationsAsync().then(async (token) => {
+            try {
+                const response = await axios.post(
+                    SERVER + "/push/token",
+                    {
+                        token,
+                    },
+                    {
+                        headers: {
+                            auth: await getAsyncStorageToken(),
+                        },
+                    }
+                );
 
-        console.log(response.data);
-      } catch (error) {
-        console.log("error : ", error);
-      }
-    });
-
-    notificationListener.current =
-      Notifications.addNotificationReceivedListener((notification) => {
-        console.log(notification);
-        setNotification(notification);
-      });
-
-    responseListener.current =
-      Notifications.addNotificationResponseReceivedListener((response) => {
-        console.log(response);
-      });
-
-    //init
-    setAcceptOrderList([]);
-    setRegistOrderList([]);
-    setLoading(true);
-
-    //작업 예약 중인 리스트 먼저 받아오기
-    getAcceptOrderList();
-
-    return () => {
-      if (
-        typeof notificationListener.current !== "undefined" &&
-        typeof responseListener.current !== "undefined"
-      ) {
-        Notifications.removeNotificationSubscription(
-          notificationListener.current
-        );
-        Notifications.removeNotificationSubscription(responseListener.current);
-      }
-    };
-  }, []);
-
-  //TODO: - 기사/기업 회원 예약중인 작업이 있을 시 예약시간 30분 전부터 gps 측정 시작 (5분마다 재 측정) > 거리 500m 이하 일시 (추후 추가)
-  console.log("acceptOrderList : ", acceptOrderList);
-
-  console.log("notification : ", notification);
-
-  const registerForPushNotificationsAsync = async () => {
-    let token;
-
-    if (Device.isDevice) {
-      token = (
-        await Notifications.getExpoPushTokenAsync({
-          projectId: "0d74eebd-b11d-421e-bce6-587423f34de3",
-        })
-      ).data;
-      console.log(token);
-    } else {
-      alert("Must use physical device for Push Notifications");
-    }
-
-    if (Platform.OS === "android") {
-      Notifications.setNotificationChannelAsync("default", {
-        name: "default",
-        importance: Notifications.AndroidImportance.MAX,
-        vibrationPattern: [0, 250, 250, 250],
-        lightColor: "#FF231F7C",
-      });
-    }
-
-    return token;
-  };
-  const getAcceptOrderList = async () => {
-    try {
-      const response = await axios.get(SERVER + "/works/mylist/accept", {
-        headers: {
-          auth: await getAsyncStorageToken(),
-        },
-      });
-
-      // console.log(response.data);
-
-      const {
-        data: { result },
-      } = response;
-
-      if (result === VALID) {
-        const {
-          data: {
-            data: { list },
-          },
-        } = response;
-
-        console.log(list);
-        const newList = [];
-        list.map((order, index) => {
-          if (
-            order.orderStatusId === 2 ||
-            order.orderStatusId === 3 ||
-            order.orderStatusId === 4
-          ) {
-            newList.push(order);
-          }
+                console.log(response.data);
+            } catch (error) {
+                console.log("error : ", error);
+            }
         });
 
-        setAcceptOrderList(newList);
+        notificationListener.current =
+            Notifications.addNotificationReceivedListener((notification) => {
+                console.log(notification);
+                setNotification(notification);
+            });
 
-        console.log("acceptOrderList : ", newList);
-        if (newList.length < 1) getRegistOrderList();
-        else setLoading(false);
-      } else {
-        const {
-          data: { msg },
-        } = response;
+        responseListener.current =
+            Notifications.addNotificationResponseReceivedListener(
+                (response) => {
+                    console.log(response);
+                }
+            );
 
-        console.log(msg);
-        getRegistOrderList();
-      }
-    } catch (error) {
-      console.log(error);
-      getRegistOrderList();
-    }
-  };
+        //init
+        setAcceptOrderList([]);
+        setRegistOrderList([]);
+        setLoading(true);
 
-  const getRegistOrderList = async () => {
-    try {
-      const response = await axios.get(SERVER + "/works/mylist/regist", {
-        headers: {
-          auth: await getAsyncStorageToken(),
-        },
-      });
+        //작업 예약 중인 리스트 먼저 받아오기
+        getAcceptOrderList();
 
-      // console.log(response.data);
+        return () => {
+            if (
+                typeof notificationListener.current !== "undefined" &&
+                typeof responseListener.current !== "undefined"
+            ) {
+                Notifications.removeNotificationSubscription(
+                    notificationListener.current
+                );
+                Notifications.removeNotificationSubscription(
+                    responseListener.current
+                );
+            }
+        };
+    }, []);
 
-      const {
-        data: { result },
-      } = response;
+    //TODO: - 기사/기업 회원 예약중인 작업이 있을 시 예약시간 30분 전부터 gps 측정 시작 (5분마다 재 측정) > 거리 500m 이하 일시 (추후 추가)
+    console.log("acceptOrderList : ", acceptOrderList);
 
-      if (result === VALID) {
-        const {
-          data: {
-            data: { list },
-          },
-        } = response;
+    console.log("notification : ", notification);
 
-        console.log("registlist:", list);
+    const registerForPushNotificationsAsync = async () => {
+        let token;
 
-        setRegistOrderList(list);
-        setLoading(false);
-      } else {
-        const {
-          data: { msg },
-        } = response;
+        if (Device.isDevice) {
+            token = (
+                await Notifications.getExpoPushTokenAsync({
+                    projectId: "0d74eebd-b11d-421e-bce6-587423f34de3",
+                })
+            ).data;
+            console.log(token);
+        } else {
+            alert("Must use physical device for Push Notifications");
+        }
 
-        console.log(msg);
-        setLoading(false);
-      }
-    } catch (error) {
-      console.log(error);
-      setLoading(false);
-    }
-  };
-  return (
-    <>
-      {loading ? (
-        <LoadingLayout />
-      ) : (
-        <Stack.Navigator
-          screenOptions={{
-            presentation: "modal",
-            headerShown: false,
-          }}
-        >
-          {firstLogin ? (
-            <Stack.Screen
-              name="Welcome"
-              component={Welcome}
-              options={{
-                headerShown: false,
-                headerTitleAlign: "center",
-              }}
-            />
-          ) : null}
+        if (Platform.OS === "android") {
+            Notifications.setNotificationChannelAsync("default", {
+                name: "default",
+                importance: Notifications.AndroidImportance.MAX,
+                vibrationPattern: [0, 250, 250, 250],
+                lightColor: color.main,
+                lockscreenVisibility: AndroidNotificationVisibility.PUBLIC,
+            });
+        }
 
-          {acceptOrderList.length > 0 ? (
-            <Stack.Screen
-              name="IntroOrderProgress"
-              component={OrderProgress}
-              options={{
-                headerShown: true,
-                headerTitleAlign: "center",
-              }}
-              initialParams={{
-                orderData: { ...acceptOrderList[0] },
-              }}
-            />
-          ) : null}
+        return token;
+    };
+    const getAcceptOrderList = async () => {
+        try {
+            const response = await axios.get(SERVER + "/works/mylist/accept", {
+                headers: {
+                    auth: await getAsyncStorageToken(),
+                },
+            });
 
-          {registOrderList.length > 0 ? (
-            <Stack.Screen
-              name="IntroCompleteOrder"
-              component={CompleteOrder}
-              options={{
-                title: "작업 완료 요청",
-                headerShown: true,
-                headerTitleAlign: "center",
-              }}
-              initialParams={{
-                orderData: { ...registOrderList[0] },
-              }}
-            />
-          ) : null}
-          <Stack.Screen name="TabsNavigator" component={TabsNavigator} />
-          <Stack.Screen
-            name="OrderDetail"
-            component={OrderDetail}
-            options={{
-              headerShown: true,
-              headerTitleAlign: "center",
-              headerTitle: "작업 상세 보기",
-              headerBackTitleVisible: false,
-            }}
-          />
-          <Stack.Screen
-            name="OrderProgress"
-            component={OrderProgress}
-            options={{
-              headerShown: true,
-              headerTitleAlign: "center",
-            }}
-          />
-          <Stack.Screen
-            name="CompleteOrder"
-            component={CompleteOrder}
-            options={{
-              title: "작업 완료 요청",
-              headerShown: true,
-              headerTitleAlign: "center",
-            }}
-          />
-          <Stack.Screen
-            name="OrdinaryOrderDetail"
-            component={OrdinaryOrderDetail}
-            options={{
-              title: "상세 보기",
-              headerShown: true,
-              headerTitleAlign: "center",
-            }}
-          />
-          <Stack.Screen
-            name="Payment"
-            component={Payment}
-            options={{
-              title: "결제하기",
-              headerShown: false,
-              headerTitleAlign: "center",
-            }}
-          />
-          <Stack.Screen
-            name="Charge"
-            component={Charge}
-            options={{
-              title: "결제하기",
-              headerShown: false,
-              headerTitleAlign: "center",
-            }}
-          />
-        </Stack.Navigator>
-      )}
-    </>
-  );
+            // console.log(response.data);
+
+            const {
+                data: { result },
+            } = response;
+
+            if (result === VALID) {
+                const {
+                    data: {
+                        data: { list },
+                    },
+                } = response;
+
+                console.log(list);
+                const newList = [];
+                list.map((order, index) => {
+                    if (
+                        order.orderStatusId === 2 ||
+                        order.orderStatusId === 3 ||
+                        order.orderStatusId === 4
+                    ) {
+                        newList.push(order);
+                    }
+                });
+
+                setAcceptOrderList(newList);
+
+                console.log("acceptOrderList : ", newList);
+                if (newList.length < 1) getRegistOrderList();
+                else setLoading(false);
+            } else {
+                const {
+                    data: { msg },
+                } = response;
+
+                console.log(msg);
+                getRegistOrderList();
+            }
+        } catch (error) {
+            console.log(error);
+            getRegistOrderList();
+        }
+    };
+
+    const getRegistOrderList = async () => {
+        try {
+            const response = await axios.get(SERVER + "/works/mylist/regist", {
+                headers: {
+                    auth: await getAsyncStorageToken(),
+                },
+            });
+
+            // console.log(response.data);
+
+            const {
+                data: { result },
+            } = response;
+
+            if (result === VALID) {
+                const {
+                    data: {
+                        data: { list },
+                    },
+                } = response;
+
+                console.log("registlist:", list);
+
+                setRegistOrderList(list);
+                setLoading(false);
+            } else {
+                const {
+                    data: { msg },
+                } = response;
+
+                console.log(msg);
+                setLoading(false);
+            }
+        } catch (error) {
+            console.log(error);
+            setLoading(false);
+        }
+    };
+    return (
+        <>
+            {loading ? (
+                <LoadingLayout />
+            ) : (
+                <Stack.Navigator
+                    screenOptions={{
+                        presentation: "modal",
+                        headerShown: false,
+                    }}
+                >
+                    {firstLogin ? (
+                        <Stack.Screen
+                            name="Welcome"
+                            component={Welcome}
+                            options={{
+                                headerShown: false,
+                                headerTitleAlign: "center",
+                            }}
+                        />
+                    ) : null}
+
+                    {acceptOrderList.length > 0 ? (
+                        <Stack.Screen
+                            name="IntroOrderProgress"
+                            component={OrderProgress}
+                            options={{
+                                headerShown: true,
+                                headerTitleAlign: "center",
+                            }}
+                            initialParams={{
+                                orderData: { ...acceptOrderList[0] },
+                            }}
+                        />
+                    ) : null}
+
+                    {registOrderList.length > 0 ? (
+                        <Stack.Screen
+                            name="IntroCompleteOrder"
+                            component={CompleteOrder}
+                            options={{
+                                title: "작업 완료 요청",
+                                headerShown: true,
+                                headerTitleAlign: "center",
+                            }}
+                            initialParams={{
+                                orderData: { ...registOrderList[0] },
+                            }}
+                        />
+                    ) : null}
+                    <Stack.Screen
+                        name="TabsNavigator"
+                        component={TabsNavigator}
+                    />
+                    <Stack.Screen
+                        name="OrderDetail"
+                        component={OrderDetail}
+                        options={{
+                            headerShown: true,
+                            headerTitleAlign: "center",
+                            headerTitle: "작업 상세 보기",
+                            headerBackTitleVisible: false,
+                        }}
+                    />
+                    <Stack.Screen
+                        name="OrderProgress"
+                        component={OrderProgress}
+                        options={{
+                            headerShown: true,
+                            headerTitleAlign: "center",
+                        }}
+                    />
+                    <Stack.Screen
+                        name="CompleteOrder"
+                        component={CompleteOrder}
+                        options={{
+                            title: "작업 완료 요청",
+                            headerShown: true,
+                            headerTitleAlign: "center",
+                        }}
+                    />
+                    <Stack.Screen
+                        name="OrdinaryOrderDetail"
+                        component={OrdinaryOrderDetail}
+                        options={{
+                            title: "상세 보기",
+                            headerShown: true,
+                            headerTitleAlign: "center",
+                        }}
+                    />
+                    <Stack.Screen
+                        name="Payment"
+                        component={Payment}
+                        options={{
+                            title: "결제하기",
+                            headerShown: false,
+                            headerTitleAlign: "center",
+                        }}
+                    />
+                    <Stack.Screen
+                        name="Charge"
+                        component={Charge}
+                        options={{
+                            title: "결제하기",
+                            headerShown: false,
+                            headerTitleAlign: "center",
+                        }}
+                    />
+                </Stack.Navigator>
+            )}
+        </>
+    );
 }
--- screen/Intro.js
+++ screen/Intro.js
@@ -4,120 +4,146 @@ import { FlatList, useWindowDimensions } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { color } from "../styles";
 import Button from "../component/button/Button";
+import * as Notifications from "expo-notifications";
 
 const imagePath = [
-  require(`../assets/images/intro/img_01.png`),
-  require(`../assets/images/intro/img_02.png`),
-  require(`../assets/images/intro/img_03.png`),
+    require(`../assets/images/intro/img_01.png`),
+    require(`../assets/images/intro/img_02.png`),
+    require(`../assets/images/intro/img_03.png`),
 ];
 
 const Container = styled.View`
-  flex: 1;
-  width: 100%;
-  background-color: white;
+    flex: 1;
+    width: 100%;
+    background-color: white;
 `;
 
 const Wrapper = styled.View`
-  flex: 10;
-  align-items: center;
-  margin-top: 120px;
+    flex: 10;
+    align-items: center;
+    margin-top: 120px;
 `;
 
 const Bottom = styled.View`
-  flex: 1.5;
+    flex: 1.5;
 `;
 
 const IntroImage = styled.Image`
-  width: ${(props) => props.size + "px"};
-  height: ${(props) => props.size + "px"};
+    width: ${(props) => props.size + "px"};
+    height: ${(props) => props.size + "px"};
 `;
 
 const Indicators = styled.View`
-  flex-direction: row;
-  justify-content: center;
-  margin-bottom: 35px;
+    flex-direction: row;
+    justify-content: center;
+    margin-bottom: 35px;
 `;
 
 const Indicator = styled.TouchableOpacity`
-  width: ${(props) => (props.cur ? "35px" : "13px")};
-  height: 13px;
-  background-color: ${(props) => props.color || color.main};
-  justify-content: center;
-  align-items: center;
-  border-radius: 100px;
-  margin: 0px 5px 0px 5px;
+    width: ${(props) => (props.cur ? "35px" : "13px")};
+    height: 13px;
+    background-color: ${(props) => props.color || color.main};
+    justify-content: center;
+    align-items: center;
+    border-radius: 100px;
+    margin: 0px 5px 0px 5px;
 `;
 
 const Buttons = styled.View`
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-evenly;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-evenly;
 `;
 
 function Intro() {
-  const { width: imageSize } = useWindowDimensions();
-  const navigation = useNavigation();
-  const flatListRef = useRef();
-  const [imageIndex, setImageIndex] = useState(0);
+    const { width: imageSize } = useWindowDimensions();
+    const navigation = useNavigation();
+    const flatListRef = useRef();
+    const [imageIndex, setImageIndex] = useState(0);
 
-  const renderIntro = ({ item: path }) => (
-    <IntroImage size={imageSize} source={path} />
-  );
+    const renderIntro = ({ item: path }) => (
+        <IntroImage size={imageSize} source={path} />
+    );
 
-  const scrollToIntroImage = (index) => {
-    flatListRef.current.scrollToIndex({ index });
-    setImageIndex(index);
-  };
+    const scrollToIntroImage = (index) => {
+        flatListRef.current.scrollToIndex({ index });
+        setImageIndex(index);
+    };
 
-  const goToSignIn = () => {
-    navigation.navigate("SignInNavigator");
-  };
+    const goToSignIn = () => {
+        navigation.navigate("SignInNavigator");
+    };
 
-  const goToSignUp = () => {
-    navigation.navigate("SignUpNavigator");
-  };
-  return (
-    <Container>
-      <Wrapper>
-        <FlatList
-          horizontal
-          pagingEnabled
-          showsHorizontalScrollIndicator={false}
-          data={imagePath}
-          renderItem={renderIntro}
-          ref={flatListRef}
-          onMomentumScrollEnd={(event) => {
-            const index = Math.floor(
-              Math.floor(event.nativeEvent.contentOffset.x) /
-                Math.floor(event.nativeEvent.layoutMeasurement.width)
-            );
-            setImageIndex(index);
-          }}
-        />
-        <Indicators>
-          {imagePath.map((__, index) => (
-            <Indicator
-              key={index}
-              onPress={() => scrollToIntroImage(index)}
-              color={imageIndex === index ? color.btnAccent : color.btnDisable}
-              cur={imageIndex === index}
-            />
-          ))}
-        </Indicators>
-      </Wrapper>
-      <Bottom>
-        <Buttons>
-          <Button
-            onPress={goToSignIn}
-            type="accent"
-            style={{ width: 185 }}
-            text="로그인"
-          />
-          <Button onPress={goToSignUp} style={{ width: 185 }} text="회원가입" />
-        </Buttons>
-      </Bottom>
-    </Container>
-  );
+    const goToSignUp = () => {
+        navigation.navigate("SignUpNavigator");
+    };
+
+    //TODO: testcode
+    const testOnClick = async () => {
+        await Notifications.scheduleNotificationAsync({
+            content: {
+                title: "You've got mail! 📬",
+                body: "Here is the notification body",
+                data: { data: "goes here" },
+            },
+            trigger: null,
+        });
+    };
+    return (
+        <Container>
+            <Wrapper>
+                <FlatList
+                    horizontal
+                    pagingEnabled
+                    showsHorizontalScrollIndicator={false}
+                    data={imagePath}
+                    renderItem={renderIntro}
+                    ref={flatListRef}
+                    onMomentumScrollEnd={(event) => {
+                        const index = Math.floor(
+                            Math.floor(event.nativeEvent.contentOffset.x) /
+                                Math.floor(
+                                    event.nativeEvent.layoutMeasurement.width
+                                )
+                        );
+                        setImageIndex(index);
+                    }}
+                />
+                <Indicators>
+                    {imagePath.map((__, index) => (
+                        <Indicator
+                            key={index}
+                            onPress={() => scrollToIntroImage(index)}
+                            color={
+                                imageIndex === index
+                                    ? color.btnAccent
+                                    : color.btnDisable
+                            }
+                            cur={imageIndex === index}
+                        />
+                    ))}
+                </Indicators>
+            </Wrapper>
+            <Bottom>
+                <Buttons>
+                    <Button
+                        onPress={goToSignIn}
+                        // onPress={async () => {
+                        //     await testOnClick();
+                        // }} //TODO: testcode
+                        type="accent"
+                        style={{ width: 185 }}
+                        text="로그인"
+                    />
+                    <Button
+                        onPress={goToSignUp}
+                        style={{ width: 185 }}
+                        text="회원가입"
+                    />
+                </Buttons>
+            </Bottom>
+        </Container>
+    );
 }
 
 export default Intro;
