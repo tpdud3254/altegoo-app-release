a4c0682e573f62e589acdbbc2999b51d43a8f73e
seyoung Ko <tpdud3254@burninggalaxy.com>
2023-06-14 14:21:41 +0900

    상수 추가

--- constant.js
+++ constant.js
@@ -1,6 +1,6 @@
 // export const SERVER = "https://altegoo.shop";
-export const SERVER = "https://8752-121-66-109-244.ngrok-free.app";
-export const WSS_SERVER = "wss://8752-121-66-109-244.ngrok-free.app";
+export const SERVER = "https://dbce-121-66-109-244.ngrok-free.app";
+export const WSS_SERVER = "wss://dbce-121-66-109-244.ngrok-free.app";
 
 export const PAYMENT_SERVER =
     "https://master.d1p7wg3e032x9j.amplifyapp.com/payment";
@@ -10,6 +10,10 @@ export const VALID = "VALID";
 export const ORDINARY = "ORDINARY";
 export const SPECIAL = "SPECIAL";
 export const PERSON = "PERSON";
+
+//TODO: 상수 전환
+export const NORMAL = "NORMAL";
+export const DRIVER = "DRIVER";
 export const COMPANY = "COMPANY";
 
 export const TOKEN = "token";
@@ -25,3 +29,11 @@ export const REGIST_NAV = [
     "Payment",
     "RegistCompleted",
 ];
+
+export const FONTS = {
+    bold: "SpoqaHanSansNeo-Bold",
+    light: "SpoqaHanSansNeo-Light",
+    medium: "SpoqaHanSansNeo-Medium",
+    regular: "SpoqaHanSansNeo-Regular",
+    thin: "SpoqaHanSansNeo-Thin",
+};
