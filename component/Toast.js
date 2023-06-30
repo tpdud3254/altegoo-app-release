c4660fb0141bfd72ad0943eee03b4e4fe91dfc28
seyoung Ko <tpdud3254@burninggalaxy.com>
2023-06-30 09:55:13 +0900

    Toast 컴포넌트 UI 수정

--- component/Toast.js
+++ component/Toast.js
@@ -1,10 +1,12 @@
 import styled from "styled-components/native";
 import { color } from "../styles";
-import MediumText from "./text/MediumText";
 import { Ionicons } from "@expo/vector-icons";
+import { shadowProps } from "./Shadow";
+import RegularText from "./text/RegularText";
+import { Image } from "react-native";
 
 const ToastBox = styled.View`
-    background-color: ${color.sub.blue};
+    background-color: ${color["box-color-background"]};
     width: 90%;
     justify-content: center;
     align-items: center;
@@ -15,11 +17,26 @@ const ToastBox = styled.View`
 
 export const toastConfig = {
     errorToast: ({ props }) => (
-        <ToastBox>
-            <Ionicons name="warning" size={30} color={color.sub.yellow} />
-            <MediumText style={{ color: "white", marginTop: 5 }}>
+        <ToastBox style={shadowProps}>
+            <Ionicons name="warning" size={25} color={color.main} />
+            <RegularText
+                style={{ color: color["page-dark-text"], marginTop: 5 }}
+            >
                 {props}
-            </MediumText>
+            </RegularText>
+        </ToastBox>
+    ),
+    normalToast: ({ props }) => (
+        <ToastBox style={shadowProps}>
+            <Image
+                source={require("../assets/images/icons/check_circle_ON.png")}
+                style={{ width: 25, height: 25 }}
+            />
+            <RegularText
+                style={{ color: color["page-dark-text"], marginTop: 5 }}
+            >
+                {props}
+            </RegularText>
         </ToastBox>
     ),
 };
