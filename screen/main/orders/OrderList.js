3e85877aae15cde0a06a2e89727365751292f757
seyoung Ko <tpdud3254@burninggalaxy.com>
2023-09-11 18:45:10 +0900

    쓰지않는 변수 삭제

--- screen/main/orders/OrderList.js
+++ screen/main/orders/OrderList.js
@@ -41,16 +41,6 @@ const HaederWrapper = styled.View`
     width: 110px;
 `;
 
-const Select = styled.TouchableOpacity`
-    background-color: #f4f4f4;
-    flex-direction: row;
-    align-items: center;
-    justify-content: space-between;
-    padding: 9px 10px 9px 17px;
-    border-radius: 10px;
-    width: 110px;
-`;
-
 const Item = styled.View`
     width: 100%;
     margin-bottom: 30px;
@@ -189,7 +179,7 @@ function OrderList({ navigation }) {
                         style={{ width: 20, height: 20, marginRight: 5 }}
                     />
                     <BoldText style={{ fontSize: 16 }}>
-                        {scheduledOrderCount}건
+                        {scheduledOrderCount < 0 ? 0 : scheduledOrderCount}건
                         <RegularText style={{ fontSize: 16 }}>
                             {" "}
                             예정
